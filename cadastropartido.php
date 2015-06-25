b98839c0a01d82a7e5ca0828badd99fc12495c0b
Vinícius Calegari <v_calegari@hotmail.com>
2015-06-25 15:29:59 -0300

    cadastropartido
    está criando e  excluindo o partido. para editar a gente mexeu mas nao
    conseguimos fazer funcionar direito

--- cadastropartido.php
+++ cadastropartido.php
@@ -43,7 +43,7 @@ if($_GET['a'] == "1" || $_GET['a'] == ''){
         $tpl->set('sigla',$res->fields('sigla'));
         $tpl->set('numero',$res->fields('numero'));
 
-        $tpl->set('link_editar','cadastropartido.php?a=3&id='.$res->fields('id_partido'));
+        $tpl->set('link_editar' ,'cadastropartido.php?a=3&id='.$res->fields('id_partido'));
         $tpl->set('link_excluir','cadastropartido.php?a=4&id='.$res->fields('id_partido'));
         $tpl->Show('partidos_table_linha');
         $res->MoveNext();
@@ -102,7 +102,80 @@ if($_GET['a'] == "2"){
 
 //editar cadastro do partido
 if($_GET['a'] == '3'){
+
+    $partido = new Partido();
+
+    if($_POST['frmPassou'] == "OK"){
+
+        if($partido->Load("id_partido = '",$_GET['id'])."'"){
+
+            $endereco = new Endereco();
+            $endereco->Load('id_endereco='.$partido->id_endereco);
+
+            $partido->nome = strtolower($_POST['nome']);
+            $partido->presidente = $_POST['presidente'];
+            $partido->numero = $_POST['numero'];
+            $partido->estado = $_POST['estado'];
+            $partido->sigla = $_POST['sigla'];
+            $partido->deferimento = $_POST['deferimento'];
+
+
+
+
+            $endereco->cep = str_replace('-', '',$_POST['cep']);
+            $endereco->logradouro = $_POST['logradouro'];
+            $endereco->numero = $_POST['numero'];
+            $endereco->bairro = $_POST['bairro'];
+            $endereco->cidade = $_POST['cidade'];
+            $endereco->estado = $_POST['estado'];
+
+            $conn->Execute('START TRANSACTION;');
+
+            if($partido->Save() && $endereco->Save()){
+                $conn->Execute('COMMIT;');
+                $tpl->set('form_cadastro_partido', 'show-alerts');
+            }
+            else{
+                $conn->Execute('ROLLBACK;');
+                $tpl->set('gravar_err', 'show-alerts');
+            }
+        }
+
+    }
+    else{
+        $partido->Load("id_partido = '".$_GET['id']."'");
+    }
+
+    if($partido){
+
+        $endereco = new Endereco();
+        $endereco->Load('id_endereco='.$partido->id_endereco);
+        $tpl->set('nome',$partido->nome);
+        $tpl->set('presidente',$partido->presidente);
+        $tpl->set('numero',$partido->numero);
+        $tpl->set('estado',$partido->estado);
+        $tpl->set('sigla',$partido->sigla);
+        $tpl->set('deferimento',$partido->deferimento);
+        $tpl->set('cep',$endereco->cep);
+        $tpl->set('logradouro',$endereco->logradouro);
+        $tpl->set('numero',$endereco->numero);
+        $tpl->set('bairro',$endereco->bairro);
+        $tpl->set('cidade',$endereco->cidade);
+        $tpl->set($endereco->estado,'selected');
+        $tpl->Show('form_cadastro_partido');
+    }
     
 }
+if($_GET['a'] == "4"){
+    $partido = new Partido();
+    $partido->Load("id_partido = '".$_GET['id']."'");
+    $endereco = new Endereco();
+    $endereco->Load('id_endereco='.$partido->id_endereco);
 
+    $partido->Delete();
+    $endereco->Delete();
+
+    header('Location: cadastropartido.php?a=1');
+
+}
 $tpl->Show('footer');
